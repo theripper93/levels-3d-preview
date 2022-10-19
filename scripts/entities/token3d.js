8109ecb03eef0493f7fb669ba0e6b0ba5f78b154
theripper93 <tsplab@gmail.com>
2022-10-19 16:35:42 +0200

    a

--- scripts/entities/token3d.js
+++ scripts/entities/token3d.js
@@ -690,7 +690,8 @@ export class Token3D {
 
     setReticule(){
       this.enableReticule ? this.mesh.add(this.reticule) : this.mesh.remove(this.reticule);
-      this.reticule.scale.set(this.w, this.d, this.h);
+      const scale = Math.max(this.h, this.w, this.d);
+      this.reticule.scale.set(scale, scale, scale);
       this.reticule.scale.multiplyScalar(1.2);
       this.reticule.position.y = this.d/2;
     }
