438c6cfdfcc0142b04243450b7309fbe74e31f78
theripper93 <tsplab@gmail.com>
2023-01-04 22:31:21 +0100

    a

--- scripts/helpers/performancePresets.js
+++ scripts/helpers/performancePresets.js
@@ -22,12 +22,18 @@ export function setPerformancePreset(){
 
 }
 
-export function injectPresetButtons(html){
+export function injectPresetButtons(html) {
+    const dpr = window.devicePixelRatio;
+    const resMulti = game.settings.get("levels-3d-preview", "resolutionMultiplier");
+    const currentPreset = findCurrentPreset();
+    const finalResWidth = window.innerWidth * dpr * resMulti;
+    const resColor = finalResWidth > 3000 ? "red" : finalResWidth > 2000 ? "orange" : "green";
     let buttons = '';
-    for(let [k,v] of Object.entries(performancePresets)){
-        buttons += `<button class="performance-preset" data-tooltip="${game.i18n.localize(`levels3dpreview.performance.buttonTooltip.${k}`)}" data-preset="${k}"><i class="${v.icon}"></i> ${game.i18n.localize(`levels3dpreview.performance.${k}`)}</button>`;
+    for (let [k, v] of Object.entries(performancePresets)) {
+        const c = k == currentPreset
+        buttons += `<button ${c ? `style="border: 2px solid var(--color-shadow-primary);"` : ""} class="performance-preset" data-tooltip="${game.i18n.localize(`levels3dpreview.performance.buttonTooltip.${k}`)}" data-preset="${k}"><i class="${v.icon}"></i> ${game.i18n.localize(`levels3dpreview.performance.${k}`)}</button>`;
     }
-    let prestHtml = $(`<div><h3 class="border">${game.i18n.localize(`levels3dpreview.performance.settings.header`)}</h3><div class="form-group">${buttons}</div><p class="notes">${game.i18n.localize(`levels3dpreview.performance.settings.presetHint`)}</p><hr></div>`);
+    let prestHtml = $(`<div><h3 class="border">${game.i18n.localize(`levels3dpreview.performance.settings.header`)}</h3><div class="form-group">${buttons}</div><p class="notes">${game.i18n.localize(`levels3dpreview.performance.settings.presetHint`)}</p><p class="notes">${game.i18n.localize(`levels3dpreview.performance.settings.resolution`).replace("{resolution}", `<strong data-tooltip="${game.i18n.localize(`levels3dpreview.performance.settings.resolutionTooltip`)}" style="cursor: help; color: ${resColor}">${window.innerWidth * dpr * resMulti}x${window.innerHeight * dpr * resMulti}</strong>`)}</p><hr></div>`);
     prestHtml.on('click', '.performance-preset', (event) => {
         let preset = event.currentTarget.dataset.preset;
         applyPreset(preset, performancePresets[preset]);
@@ -46,6 +52,25 @@ async function applyPreset(id, preset){
     }
     SettingsConfig.reloadConfirm();
     }, 1000);
+}
+
+function findCurrentPreset() { 
+    let currentPreset = "custom";
+    for(let [k,v] of Object.entries(performancePresets)){
+        let matches = true;
+        for(let [k2,v2] of Object.entries(v)){
+            if(k2 == "icon") continue;
+            if(game.settings.get("levels-3d-preview", k2) != v2){
+                matches = false;
+                break;
+            }
+        }
+        if(matches){
+            currentPreset = k;
+            break;
+        }
+    }
+    return currentPreset;
 }
 
 
