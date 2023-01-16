4a29d9de5ef728615df07e71cbd100464c41ef29
theripper93 <tsplab@gmail.com>
2023-01-16 19:03:06 +0100

    a

--- scripts/settingsConfig.js
+++ scripts/settingsConfig.js
@@ -93,7 +93,7 @@ Hooks.once('init', function() {
     scope: "world",
     config: false,
     type: Boolean,
-    default: true,
+    default: false,
   });
 
   game.settings.register("levels-3d-preview", "gameCameraMaxAngle", {
