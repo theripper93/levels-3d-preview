d0476ac44aaceaabb66d88c7fdceaf0619cc9aed
svchost <m.mattiabaraldi@gmail.com>
2026-05-03 23:38:57 +0200

    b

--- scripts/helpers/conflict-manager.js
+++ scripts/helpers/conflict-manager.js
@@ -1,7 +1,8 @@
 export function check3DToggleConflicts() {
     let conflict = false;
     if (game.modules.get("pf2e-toolbelt")?.active) {
-        if (game.settings.get("pf2e-toolbelt", "underground.enabled")) {
+        if (game.settings.settings.has("pf2e-toolbelt.underground.enabled") &&
+            game.settings.get("pf2e-toolbelt", "underground.enabled")) {
             ui.notifications.error("3D Canvas | PF2e Toolbelt -> Undeground -> Enabled setting detected. Please disable it to avoid conflicts.", { permanent: true });
             conflict = true;
         }
